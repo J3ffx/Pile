e2bf5cf7be25e16daebb0f482a1953803e80a3c9
9h0ul <elharbilsami123@gmail.com>
2019-05-13 20:33:44 +0200

    SAMI

--- src/interpret/Interpret.java
+++ src/interpret/Interpret.java
@@ -1,4 +1,9 @@
 package interpret;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import piling.Pile;
@@ -7,31 +12,58 @@ public class Interpret {
 	private String code;
 	private ArrayList<String> list;
 	private Pile pile;
-	
-	public String[] analyse(String code) {
-		String[]words = code.split(" ");
+
+	public Interpret() {
+		pile = new Pile();
+
+	}
+
+	public ArrayList<String> analyse(String code) {
+		ArrayList<String> words = new ArrayList<String>();
+		String[] word = code.split(" ");
+		for (String elt : word)
+			words.add(elt);
 		return words;
 	}
-	
-	public boolean estValeur(String s) {
-		try { 
-	        Integer.parseInt(s); 
-	    } catch(NumberFormatException e) { 
-	        return false; 
-	    } catch(NullPointerException e) {
-	        return false;
-	    }
-	    return true;
-	}
-	
-	public int valeur(String s) {
-		return Integer.parseInt(s); 
-	}
-	
-	public void evaluate(String code) {
-		
-	}
-	
-	
 
+	public boolean estValeur(String s) {
+		try {
+			Integer.parseInt(s);
+		} catch (NumberFormatException e) {
+			return false;
+		} catch (NullPointerException e) {
+			return false;
+		}
+		return true;
+	}
+
+	public int valeur(String s) {
+		return Integer.parseInt(s);
+	}
+
+	public void evaluate() {
+		this.list = analyse(code);
+		for (String elt : this.list) {
+			if (estValeur(elt)) {
+				pile.empiler(valeur(elt));
+			} else if (elt.equals("PLUS")) {
+				pile.plus();
+			} else if (elt.equals("MULT"))
+				pile.mult();
+			else {
+				System.out.println(elt);
+			}
+
+		}
+
+	}
+
+	public void execute(String filename) throws IOException {
+		FileReader file = new FileReader(filename);
+		BufferedReader br = new BufferedReader(file);
+		this.code = br.readLine();
+		evaluate();
+		pile.afficher();
+		br.close();
+	}
 }
--- src/piling/main.java
+++ src/piling/main.java
@@ -1,15 +1,14 @@
 package piling;
 
+import java.io.IOException;
+
+import interpret.Interpret;
+
 public class main {
 
-	public static void main(String[] args) {
-		Pile p=new Pile();
-		p.empiler(1);
-		p.empiler(2);
-		p.plus();
-		p.empiler(3);
-		p.mult();
-		p.afficher();
+	public static void main(String[] args) throws IOException {
+		Interpret i=new Interpret();
+		i.execute("a.txt");
 	}
 
 }
