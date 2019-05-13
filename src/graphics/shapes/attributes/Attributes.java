23086e6ceb61f51a8346c8b5ea99e72e1ef05ea7
Jérôme Fanfan <jeromexfanfan@gmail.com>
2019-05-13 17:49:44 +0200

    Quick Commit Fanfan

--- src/graphics/shapes/attributes/Attributes.java
+++
@@ -1,8 +0,0 @@
-package graphics.shapes.attributes;
-
-public abstract class Attributes {
-	@SuppressWarnings("unused")
-	private int id;
-
-	public abstract String getId();
-}
---
+++ src/piling/pile.java
@@ -0,0 +1,5 @@
+package piling;
+
+public class pile {
+
+}
