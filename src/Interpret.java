43d930beab55f467c1e4d76f007722f74e1bac9f
9h0ul <elharbilsami123@gmail.com>
2019-05-13 20:03:50 +0200

    SAMI

---
+++ src/Interpret.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Interpret {
+	private String code;
+	private ArrayList<String> list;
+	public String[] analyse(String code) {
+		String[]words = code.split(" ");
+		return words;
+		
+	}
+	
+	
+
+}
---
+++ src/interpret/Plus.java
@@ -0,0 +1,18 @@
+package interpret;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Plus extends Command {
+
+	public Plus() {
+		super.setName("Plus");
+	}
+	@Override
+	public void execute(Processor p) throws IOException, ProcessorException {
+		ArrayList<Integer> ar=new ArrayList<Integer>();
+		
+		
+	}
+
+}
