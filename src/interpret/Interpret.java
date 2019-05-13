4b1fb9b1439be124351cf92c5be09ef71028b42c
Jérôme Fanfan <jeromexfanfan@gmail.com>
2019-05-13 20:11:58 +0200

    Quick Commit Fanfan

---
+++ src/interpret/Interpret.java
@@ -0,0 +1,37 @@
+package interpret;
+import java.util.ArrayList;
+
+import piling.Pile;
+
+public class Interpret {
+	private String code;
+	private ArrayList<String> list;
+	private Pile pile;
+	
+	public String[] analyse(String code) {
+		String[]words = code.split(" ");
+		return words;
+	}
+	
+	public boolean estValeur(String s) {
+		try { 
+	        Integer.parseInt(s); 
+	    } catch(NumberFormatException e) { 
+	        return false; 
+	    } catch(NullPointerException e) {
+	        return false;
+	    }
+	    return true;
+	}
+	
+	public int valeur(String s) {
+		return Integer.parseInt(s); 
+	}
+	
+	public void evaluate() {
+		
+	}
+	
+	
+
+}
