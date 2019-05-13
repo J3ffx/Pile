d3c05f117b304a46d3a6d96c992b7e37003cbdc2
Jérôme Fanfan <jeromexfanfan@gmail.com>
2019-05-13 19:28:59 +0200

    Quick Commit Fanfan

--- src/piling/pile.java
+++ src/piling/pile.java
@@ -1,12 +1,12 @@
 package piling;
 
-public class pile {
+public class Pile {
 	
 	private int []tab;
 	private int sommet;
 	static private int MAX=1000;
 	
-	public pile() {
+	public Pile() {
 		this.sommet=-1;
 		this.tab=new int[MAX];
 	}
@@ -16,14 +16,18 @@ public class pile {
 		for(int index:tab)
 		System.out.println(index);
 	}
+	
 	public void empiler(int element) {
 		this.sommet++;
-		this.tab[sommet]=element;
-						
+		this.tab[sommet]=element;			
 	}
-	public int sumall(int[]tab) {
-		
-		return (Integer) null;
+	
+	public int sumall() {
+		int sum = 0;
+		for(int elt:this.tab) {
+			sum+=elt;
+		}
+		return sum;
 	}
 	
 
